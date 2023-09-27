0e7fa25afe398bbd03a1766828323edeb03dbf39
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-09-27 22:27:38 +0300

    BrowserRouter, Routes and Route are added in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 // import reactLogo from "./assets/react.svg";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
     <div>
-      <h1>Travel diary</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
