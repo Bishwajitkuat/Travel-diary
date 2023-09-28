a37d66b7546bc50b6cd104fe9824baf4d0861c4b
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-09-28 20:24:06 +0300

    data is featched and stroed in states

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,28 @@ import styles from "./css/AppLayout.module.css";
 import Login from "./pages/Login";
 import AppLayout from "./components/AppLayout";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://localhost:8000/cities");
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div className={styles.app}>
       {/* all the Routes and Route has to be wrapped by BrowserRouter */}
@@ -31,7 +51,10 @@ function App() {
             {/* index route will be the default child route if parent route is visited
             in other words: parent ruoute's element-component's child is <Oullet/> and it's defaul value comes form index route's element component */}
             <Route index element={<p>Index route</p>} />
-            <Route path="cities" element={<CityList />} />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>List of countries</p>} />
             <Route path="form" element={<p>form</p>} />
           </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import styles from "../css/CityList.module.css";
+import Spinner from "./Spinner";
 
-function CityList() {
-  return <div className={styles.CityList}>list of cities</div>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  return (
+    <div className={styles.CityList}>
+      {cities.map((item) => (
+        <p key={item.cityName}>{item.cityName}</p>
+      ))}
+    </div>
+  );
 }
 
 export default CityList;
