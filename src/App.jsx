fd652170ec7879efea9c124f9545de9c486c6757
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-09-29 19:20:22 +0300

    all the state and logics are transfered from App componet to CitiesContext and removed all the props passing from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import reactLogo from "./assets/react.svg";
 
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { CitiesContextProvider } from "./contexts/CitiesContext";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -16,64 +16,41 @@ import City from "./components/City";
 import Form from "./components/Form";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch("http://localhost:8000/cities");
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        alert(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <div className={styles.app}>
-      {/* all the Routes and Route has to be wrapped by BrowserRouter */}
-      <BrowserRouter>
-        {/* Navbar or anything that will be visible in each page should be inside
+    <CitiesContextProvider>
+      <div className={styles.app}>
+        {/* all the Routes and Route has to be wrapped by BrowserRouter */}
+        <BrowserRouter>
+          {/* Navbar or anything that will be visible in each page should be inside
         the "BrowserRouter" but outside of "Routes" */}
-        {/* <PageNav /> */}
-        {/* all the "Route" must be wrapped inside "Routes" only one "Route" can be render at a time */}
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/login" element={<Login />} />
-          {/* nested rout: parent route app  */}
-          <Route path="/app" element={<AppLayout />}>
-            {/* child routes */}
-            {/* index route will be the default child route if parent route is visited
+          {/* <PageNav /> */}
+          {/* all the "Route" must be wrapped inside "Routes" only one "Route" can be render at a time */}
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/login" element={<Login />} />
+            {/* nested rout: parent route app  */}
+            <Route path="/app" element={<AppLayout />}>
+              {/* child routes */}
+              {/* index route will be the default child route if parent route is visited
             in other words: parent ruoute's element-component's child is <Oullet/> and it's defaul value comes form index route's element component */}
-            {/* <Route
+              {/* <Route
               index
               element={<CityList cities={cities} isLoading={isLoading} />}
             /> */}
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="cities/:id" element={<City cities={cities} />} />
-            <Route
-              path="countries"
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="form" element={<Form />} />
-          </Route>
-          {/* if url does not matche any Route then "*" Route will be rendered */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            {/* if url does not matche any Route then "*" Route will be rendered */}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </CitiesContextProvider>
   );
 }
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
 const CitiesContextProvider = ({ children }) => {
-  const data = {};
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://localhost:8000/cities");
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  const data = { cities, setCities, isLoading, setIsLoading };
   return (
     <CitiesContext.Provider value={data}>{children}</CitiesContext.Provider>
   );
