057a6210ff2581322a232c657c9ec5cdc75dfbc3
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-10-01 00:54:20 +0300

    passing lat and lng value through url prams to form path

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -73,7 +73,7 @@ const DetectClick = () => {
   const navigate = useNavigate();
   // useMapEvents() is a hook provided by leaflet, which takes argument as an opject where key is the event name and value is call back function for some action
   useMapEvents({
-    click: (e) => navigate(`form`),
+    click: (e) => navigate(`form?${e.latlng.lat}&${e.latlng.lng}`),
   });
 };
 
